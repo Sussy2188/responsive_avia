79fe2d1dad2fcce2c7bcf286aef17989d7860992
Jaqueline Arriaza <Sussy@mbp-de-walter.lan>
2018-09-18 21:12:16 -0600

    “cambio”

--- js/general.js
+++ js/general.js
@@ -1,6 +1,10 @@
 var $ = jQuery;
 
 $(document).ready(function(){
+
+  $('.lens-icon').on('click', function(){
+    $( ".search-form" ).toggleClass('open');
+  });
   
   // set middle div content height as first image in row
   middleTextHeight();
@@ -14,23 +18,21 @@ $(document).ready(function(){
   });
 
   // Swiper 
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false
-    },
-    loop:true
-  })
+  
 });
+var mySwiper;
+function swiperResize(slides){
+  var ww = $(document).width()
+  if (ww>1000) mySwiper.params.slidesPerView = slides;
+  if (ww>760 && ww<=1000) mySwiper.params.slidesPerView = 3;
+  if (ww>468 && ww<=760) mySwiper.params.slidesPerView = 2;
+  if (ww<=468) mySwiper.params.slidesPerView = 1;
+  mySwiper.update();
+}
 
 function setSwiper(slides) {
   // Swiper 
-  var mySwiper = new Swiper('.swiper-container', {
+    mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     slidesPerView: slides,
     spaceBetween: 30,
@@ -44,24 +46,32 @@ function setSwiper(slides) {
     },
     loop: true
   });
+  
+  $(window).resize(function() {swiperResize(slides)});
+  setTimeout(function() {
+    swiperResize(slides);
+  },1);
 }
-function middleTextHeight() {
-  var second_section_h = $('.second-section__left').height();
-  $('.second-section__middle').height(second_section_h);
 
-  $( window ).resize(function() {
-    var second_section_h = $('.second-section__left').height();
-    $('.second-section__middle').height(second_section_h);
+
+
+function middleTextHeight() {
+  var second_section_h = $('.second-section__left img').height();
+  $('.second-section__middle').height(second_section_h);
+  $(window).resize(function() {
+  var second_section_h = $('.second-section__left img').height();
+  $('.second-section__middle').height(second_section_h);
   });
-}
+  }
+  
 
 
 
 // Get the header
-var header = document.getElementById("header");
+var header = $("#header");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = header.offset().top;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function headerSticky() {
