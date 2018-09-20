b05b48b9441a2f0d90a143caf611b5bb73556c6a
“Sussy2188git config --global user.email “susiarriaza95@gmail.com <arriaza.jaqueline95@gmail.com>
2018-09-20 15:57:40 -0600

    “cambio”

--- page-centro-convenciones.php
+++ page-centro-convenciones.php
@@ -11,23 +11,24 @@
     <div class="first-section__wrapper">
       <div class="hero-image"></div>
       <div class="cover-text col-xs-11 col-sm-8 col-md-4 col-lg-4">
-        <h2 class="first-section__content-title">PRONTO EN AVIA</h2>
-        <h2 class="first-section__content-subtitle">EPICA EVENTOS Y 
+        <h2 class="second-section__content-title">PRONTO EN AVIA</h2>
+        <h2 class="second-section__content-subtitle">EPICA EVENTOS Y 
           CONVENCIONES</h2>
         <p class="first-section__content-text">Celebraciones, conciertos al aire libre,
-        exposiciones, pasarelas, tours, talleres y 
-        eventos corporativos encuentran el 
+        exposiciones, pasarelas,
+         tours, talleres y eventos 
+         corporativos encuentran el 
         escenario perfecto.
       </p>
       </div>
       <div class="col-xs-12 col-sm-12 direction ">
-        <h2 class="col-xs-12 col-sm-12 first-section__content-title ">DETALLES</h2>
-        <p class="col-xs-12 col-sm-12 first-section__content-parrafo">Un espacio moderno y vers&aacute;til que puede recibir a una audiencia macro de</p>
-        <p class="col-xs-12 col-sm-12 first-section__content-parrafo">hasta 600 personas, o a grupos menores de hasta 150.</p>
-        <h2 class="col-xs-12 col-sm-12 first-section__content-title">
-          </br> CONTACTO</h2>
-        <p class="col-xs-12 col-sm-12 first-section__content-parrafo">T. 2217-6017</p>
-        <p class="col-xs-12 col-sm-12 first-section__content-parrafo">C. ccavia@ghlhoteles.com</p>
+        <h2 class="col-xs-12 col-sm-12 second__content-title" style="font-size: 17px; padding-bottom: 19px;"  >DETALLES</h2>
+        <p class="col-xs-12 col-sm-12 first-section__content-parrafo">Apartamentos de uno a tres dormitorios,
+          entre 150 y 250m2 5 s&oacute;tanos de 
+          estacionamiento Phenthouse</p>
+        <h2 class="col-xs-12 col-sm-12 second__content-title " style="font-size: 17px; padding-bottom: 19px;  padding-top: 50px;">CONTACTO</h2>
+        <p class="col-xs-12 col-sm-12 first-section__content-parrafo">T. 2217-6017
+            C. ccavia@ghlhoteles.com</p>
       </div>
     </div>
   </section>
